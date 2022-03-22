3d4fa814faf3655f563b5f4a1a7bc9fad8b77906
Mitchel Wachtel <mitchel.wachtel@gmail.com>
2022-03-22 15:39:33 -0400

    got selectEmp to be async. Keep that pattern for the rest of the functions

--- helpers/empFuns.js
+++ helpers/empFuns.js
@@ -1,8 +1,22 @@
 const inquirer = require("inquirer");
 const cTable = require("console.table");
-const db = require("../config/connection");
 
+async function selectEmp() {
+  const mysql = require("mysql2/promise");
 
+  const db = await mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "rootroot",
+    database: "system_db",
+  });
 
-module.exports = {addEmployee, viewAllEmployees, updateEmployeeRole};
+  const [rows, fields] = await db.execute(
+    "SELECT * FROM employee"
+  );
+  console.log("\n")
+  console.table(rows);
+  console.log("\n\n\n\n\n\n\n");
+}
 
+module.exports = {selectEmp};
--- testing.js
+++ testing.js
@@ -1,8 +1,27 @@
 const cTable = require("console.table");
-const db = require("./config/connection");
+// const db = require("./config/connection");
 
-db.query(`SELECT id FROM employee WHERE first_name = 'Susie'`, function (err, result) {
-  
-  console.log(result[0]['id']);
-  
-});
+// db.query(`SELECT id FROM employee WHERE first_name = 'Susie'`, function (err, result) {
+
+//   console.log(result[0]['id']);
+
+// });
+
+async function main() {
+  const mysql = require("mysql2/promise");
+
+  const db = await mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "rootroot",
+    database: "system_db",
+  });
+
+  const [rows, fields] = await db.execute(
+    "SELECT * FROM employee"
+  );
+
+  console.table(rows);
+}
+
+main();
